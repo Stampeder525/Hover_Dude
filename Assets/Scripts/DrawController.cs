9d1ea0328773ca9824567bf3dee77a69f535e9f1
Loren Heubert-Aubry <lorenha@umich.edu>
2018-10-13 02:49:01 -0400

    added dynamic lerp fraction

--- Assets/Scripts/DrawController.cs
+++ Assets/Scripts/DrawController.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class DrawController : MonoBehaviour {
     public float radius;
     private Vector3 cursorPos;
-    private float lerpSpeed;
+    private float lerpFract;
     private float angle;
 
     // Use this for initialization
     void Start () {
 		cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //CHANGE MOUSE TO CONTROLLER INPUT
-        lerpSpeed = 0.4f;
-        angle = cursorPos.x;
+        lerpFract = 0.3f;
+
+        angle = Vector3.Angle(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.parent.position, transform.position - transform.parent.position);
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        cursorPos = Vector3.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.1f); //CHANGE MOUSE TO CONTROLLER INPUT
-        angle += cursorPos.x;
+        Vector3 mouseVec = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.parent.position;
+        Vector3 ballVec = transform.position - transform.parent.position;
+        angle = Vector2.Angle(mouseVec,ballVec);
+        if (angle >= 30)
+        {
+            lerpFract = 0.1f;
+        }
+        else
+        {
+            lerpFract = 0.3f;
+        }
+        Debug.Log("Angle: " + angle + ", LerpFract: " + lerpFract);
+        cursorPos = Vector3.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), lerpFract); //CHANGE MOUSE TO CONTROLLER INPUT
         Vector2 offset = (Vector2)cursorPos - Vector2.zero;
         offset.Normalize();
         offset = offset * radius;
