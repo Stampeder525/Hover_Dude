8cd618ad7c48fa558354e5a36e50d09699fe04f5
Loren Heubert-Aubry <lorenha@umich.edu>
2018-10-13 00:43:57 -0400

    added trail and line renderers

---
+++ Assets/Scripts/DrawController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawController : MonoBehaviour {
+    public float radius;
+    private Vector3 cursorPos;
+
+    // Use this for initialization
+    void Start () {
+		cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //CHANGE MOUSE TO CONTROLLER INPUT
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //CHANGE MOUSE TO CONTROLLER INPUT
+        //angle += cursorPos.x;
+        Vector2 offset = (Vector2)cursorPos - Vector2.zero;
+        offset.Normalize();
+        offset = offset * radius;
+        transform.position = offset;
+        Vector2 dir = cursorPos - this.transform.position;
+        //Check if dir is negative, then reverse
+        if (Mathf.Abs(cursorPos.x) <= 2f && Mathf.Abs(cursorPos.y) <= 2f)
+        {
+            dir = -1 * dir;
+        }
+    }
+}
