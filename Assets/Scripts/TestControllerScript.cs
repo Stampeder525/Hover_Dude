4ed6ef08263f9ddf4ce2fa99b7272bfa844bb5db
Paul DeVito <pid7@cornell.edu>
2018-10-13 02:54:04 -0400

    simple controller input

---
+++ Assets/Scripts/TestControllerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class TestControllerScript : MonoBehaviour {
+	public InputDevice inputDevice;
+
+	// Use this for initialization
+	void Start () {
+		// inputDevice = InputManager.ActiveDevice;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var activeDevice = InputManager.ActiveDevice;
+		print(activeDevice.LeftStickX);
+		transform.Rotate(Vector3.down, 500.0f * Time.deltaTime * activeDevice.LeftStickX, Space.World);
+	}
+}
